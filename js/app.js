7b6b03d0284d37885ab92b8a7269d68271b49229
MilagrosQB <qbmilagros@gmail.com>
2016-10-18 10:51:48 -0500

    version 3

--- js/app.js
+++ js/app.js
@@ -19,5 +19,45 @@ window.addEventListener("load",function(){
 		addList.style.display = "none";
 	}
 
+	btnSave.addEventListener("click",function(e){
+		e.preventDefault();
+		imprimirCard();
+		addInput.value = "";
+	});
+
+	btnCerrar.addEventListener("click", function(e){
+		e.preventDefault();
+		containerFather.style.display = "none";
+		addList.style.display = "block";
+		addInput.value = "";
+	})
+
+	function imprimirCard(){
+		var lista = document.createElement("div");
+		var inputValue = document.createTextNode(addInput.value);
+		var agregarNuevaTarjeta = document.createElement("a");
+		var textTarjeta = document.createTextNode("Añadir una tarjeta ...");
+		
+		agregarLista();
+		function agregarLista(){
+				containerFather.style.display = "none";
+			var divNew = document.createElement("div");
+				containerGrand.appendChild(divNew);
+				divNew.style.display = "inline-block";
+				divNew.appendChild(addList);
+				addList.style.display = "inline-block";
+				lista.appendChild(inputValue);
+				lista.classList.add("lista");
+				containerGrand.appendChild(lista);
+				lista.style.display = "inline-block";
+				agregarNuevaTarjeta.appendChild(textTarjeta);
+				lista.appendChild(agregarNuevaTarjeta);
+				agregarNuevaTarjeta.classList.add("tarjeta");
+				containerGrand.appendChild(addList);
+				containerGrand.appendChild(formCard);
+		}
+
+	
+	}
 });
 
